1ac1f55a2369d97a749c6622109c246b6ed41f56
Hugo Posnic <hugo.posnic@gmail.com>
2020-12-11 19:47:14 +0100

    Just replace path by filename in a message

--- src/window.py
+++ src/window.py
@@ -230,7 +230,7 @@ class CurtailWindow(Gtk.ApplicationWindow):
         elif Path.is_file(path):
             if not self.check_extension(path):
                 message_dialog(self, 'error', _("Format not supported"),
-                    _("The format of {} is not supported.").format(filename))
+                    _("The format of {} is not supported.").format(path.name))
             else:
                 verified_filenames.append(filename)
         else:
