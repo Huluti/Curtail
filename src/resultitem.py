5cd4d9da9434f8292213b92385bb282bfd986324
ARAKHNID <arakhnid@protonmail.com>
2026-03-01 10:24:32 -0600

    Use GFile instead of pathlib
    This allows us to get the host path from the portal, which is necessary
    for #185.

--- src/resultitem.py
+++ src/resultitem.py
@@ -1,11 +1,12 @@
 # resultitem.py
 
-from gi.repository import GObject
+from gi.repository import Gio, GObject
 
 from .tools import sizeof_fmt
 
 
 class ResultItem(GObject.Object):
+    file = GObject.Property(type=Gio.File)
     name = GObject.Property(type=str)
     filename = GObject.Property(type=str)
     new_filename = GObject.Property(type=str)
@@ -19,9 +20,10 @@ class ResultItem(GObject.Object):
     error = GObject.Property(type=bool, default=False)
     error_message = GObject.Property(type=str, default="")
 
-    def __init__(self, name, filename, new_filename, size):
+    def __init__(self, file, name, filename, new_filename, size):
         super().__init__()
 
+        self.file = file
         self.name = name
         self.filename = filename
         self.new_filename = new_filename
