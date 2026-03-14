5138cddb374af91a7000c23f0c6cc7a06ccc1b15
Hugo Posnic <hugo.posnic@protonmail.com>
2026-03-14 13:34:13 +0100

    Some fixes

--- src/compression_manager.py
+++ src/compression_manager.py
@@ -16,14 +16,14 @@ class CompressionManager:
         if mime_type not in self.compressors:
             self.compressors[mime_type] = ConcreteCompressor(self.settings)
 
-    def compress(self, result_items, update_result_item, enable_compression):
+    def compress(self, result_items, c_update_result_item, c_enable_compression):
         threading.Thread(
             target=self._compress,
-            args=(result_items, update_result_item, enable_compression),
+            args=(result_items, c_update_result_item, c_enable_compression),
             daemon=True,
         ).start()
 
-    def _compress(self, result_items, update_result_item, enable_compression):
+    def _compress(self, result_items, c_update_result_item, c_enable_compression):
         cpu_count = os.cpu_count()
         executor = ThreadPoolExecutor(max_workers=cpu_count)
         futures = []
@@ -33,9 +33,9 @@ class CompressionManager:
                 # Must be manually skipped
                 if not self.do_new_file:
                     self.create_tmp_result_item(result_item)
-            future = executor.submit(self.compressors[file_type].run, result_item)
+            future = executor.submit(self.compressors[file_type].run, result_item, c_update_result_item)
             futures.append(future)
 
         for future in futures:
             future.result()
-        GLib.idle_add(enable_compression, True)
+        GLib.idle_add(c_enable_compression, True)
